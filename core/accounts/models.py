c564de1752cdbea018e22aa2425913770b5d44cc
roham <sajjadkarimi766@gmail.com>
2025-09-05 14:43:37 +0330

    config the accounts models

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,14 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import(BaseUserManager,AbstractBaseUser,PermissionsMixin)
+from django.utils.translation import ugettext_lazy as _
+
+class UserManager(BaseUserManager):
+    '''
+    user manager for custom model
+    '''
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError(("Email can't be Empty"))
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('super user must have is_staff = true'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(('super user must have is_superuser = true'))
+        return self.create_user(email,password,**extra_fields)
 
 class User(AbstractBaseUser,PermissionsMixin):
+    '''
+    custom user model
+    '''
     email = models.EmailField(max_length=250,unique=True)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     #is_verified = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
