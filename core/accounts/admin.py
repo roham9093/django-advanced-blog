3a6232d7c896e1d1ca5b4bff094852ac6cdd6205
roham <sajjadkarimi766@gmail.com>
2025-09-05 15:23:35 +0330

    develop the admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    '''
+    show the table of accounts model in Data Base
+    '''
+    model = User
+    list_display = ('email', 'is_active', 'is_superuser')
+    list_filter = ('email', 'is_active', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authentications', {
+            "fields": (
+                'email','password'
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff','is_active','is_superuser'
+            ),
+        }),
+    )
+    add_fieldsets = (
+        ('new', {
+            'classes':('wide',),
+            'fields': (
+                'email','password1','password2','is_staff','is_active','is_superuser'
+            ),
+        }),
+    )
+    
+
+admin.site.register(User,CustomUserAdmin)    
