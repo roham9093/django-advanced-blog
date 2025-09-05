9c57e8ebfcb3fb24e9f6811d58a19d53bcc0add6
roham <sajjadkarimi766@gmail.com>
2025-09-05 13:45:24 +0330

    config the user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import(BaseUserManager,AbstractBaseUser,PermissionsMixin)
 
-# Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=250,unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    #is_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.email
+    
